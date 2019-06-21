1365ffba309b2c485e5998a32ae0569ede5855fe
rodinhart@gmail.com <rodin.hart@concentra.co.uk>
2019-06-21 21:57:11 +0100

    edn

---
+++ cook/recipes.clj
@@ -0,0 +1,32 @@
+[
+  {
+    :ingredients [
+      ["300g plain flour"
+      "pinch of salt"
+      "50g butter, in cubes and chilled"]
+      ["120g butter, grated and frozen"
+      "4 to 6 tbsp water"]
+    ]
+
+    :title "cheat's puff pastry"
+
+    :method [
+      "This is Paul Hollywood's recipe, and it just works.
+      It allows you to make a lovely flaky pastry with a little forthought
+      (freezing and grating the butter), and about 30 minutes of preparation time."
+
+      "Put the flour, salt and butter cubes in a bowl and break down the butter using
+      fingertips until it resembles breadcrumbs. Without too much kneading,
+      bring it together to form a soft dough."
+
+      "Roll out the dough into a rectangle about 8cm wide and 24cm high.
+      Put half the grated butter on the bottom two thirds of the rectangle,
+      fold the top third over, and then the bottom third, including the butter, 
+      on top of that. Roll out the dough again, and repeat with the other half of
+      the butter. Roll out a final time, and fold it in the same way but without 
+      putting butter in this time."
+
+      "Package the dough in baking parchment and leave to chill in the fridge for at least an hour. Done!"
+    ]
+  }
+]
