73b4231f9d04d2cc124c94ec4cb910ce7d51ab90
rodinhart@gmail.com <rodin.hart@concentra.co.uk>
2019-06-26 08:25:01 +0100

    update

--- cook/recipes.clj
+++ cook/recipes.clj
@@ -1,5 +1,7 @@
 [
   {
+    :title "cheat's puff pastry"
+
     :ingredients [
       ["300g plain flour"
       "pinch of salt"
@@ -7,8 +9,6 @@
       ["120g butter, grated and frozen"
       "4 to 6 tbsp water"]
     ]
-
-    :title "cheat's puff pastry"
 
     :method [
       "This is Paul Hollywood's recipe, and it just works.
@@ -28,5 +28,178 @@
 
       "Package the dough in baking parchment and leave to chill in the fridge for at least an hour. Done!"
     ]
+  }
+  {
+    :title "coleslaw with curry"
+
+    :ingredients [
+      ["couple of good handful of raisins, soaked in warm water",
+      "half a pointed cabbage, finely sliced",
+      "two apples, decored and cut into small pieces"]
+      ["mayonnaise", "yogurt", "curry powder", "ground cumin"]
+    ]
+
+    :method [
+      "This twist on the traditional coleslaw is a real favourite,
+      and everyone in the family knows how to make it. Adding
+      curry to a coleslaw might seem somewhat insane, but it works
+      surprisingly well, and the dish makes an excellent side."
+
+      "Put the cabbage and apple in a large bowl. Drain the raisins and add to the bowl."
+
+      "In a separate bowl, mix equal amounts of mayonnaise and yogurt, about 3 tablespoons each. Add about a teaspoon
+        of curry powder
+        and a pinch of cumin. Finally, mix the dressing into the big bowl."
+
+      "The amount dressing should be adjusted to the size of the pointed cabbage and to taste. Similar, the ratio of
+        yogurt
+        and mayonnaise can be altered."
+    ]
+  }
+  {
+    :title "cucumber salad"
+
+    :ingredients [
+      ["2 to 3 Persian cucumbers" "sea salt" "&#x00bd clove garlic"
+      "2 tbsp extra virgin olive oil"]
+      ["1 tbsp red wine vingar" "Freshly ground black pepper"
+      "Good pinch of dried oregano"]
+    ]
+
+    :method [
+      "I pinched this lovely salad from a Stanley Tucci cookbook."
+
+      "With the tines of a fork, score the cucumbers to create some texture and then cut
+        them crosswise into
+        pennies. Put a pinch of salt in the bottom of a salad bowl and, using the garlic clove, rub it all over the
+        bowl.
+        Discard the garlic. Place the cucumber slices in the bowl."
+
+      "In a small bowl, whisk together the olive oil, vinegar, a pinch of salt, and pepper
+        until you have an
+        emulsified dressing. Pour enough of it over the cucumbers to coat lightly. Sprinkle with the oregano and serve
+        at once."
+    ]
+  }
+  {
+    :title "goulash with fennel"
+  }
+  {
+    :title "red cabbage"
+  }
+  {
+    :title "cottage pie"
+  }
+  {
+    :title "mashed potatoes"
+  }
+  {
+    :title "curly kale mash"
+  }
+  {
+    :title "leek and bacon pie"
+  }
+  {
+    :title "roti"
+  }
+  {
+    :title "apple pie"
+  }
+  {
+    :title "salami fried rice "
+  }
+  {
+    :title "chili, broccoli, feta and sausage"
+  }
+  {
+    :title "quiche"
+  }
+  {
+    :title "chicken, white wine and mushrooms"
+  }
+  {
+    :title "brusselsprout stirfry"
+  }
+  {
+    :title "pasta with spinash, garlic and parmasan"
+  }
+  {
+    :title "spelt tartlet"
+  }
+  {
+    :title "spiced chicken curry with yogurt"
+  }
+  {
+    :title "snert (split pea soup)"
+  }
+  {
+    :title "cabbage stirfry"
+  }
+  {
+    :title "roti with chicken (pieces)"
+  }
+  {
+    :title "bagels"
+  }
+  {
+    :title "pita breads"
+
+    :method [
+      "Making pita breads is so easy, I wonder why I didn't tweak it before."
+    ]
+  }
+  {
+    :title "french toast with yogurt cream"
+  }
+  {
+    :title "Crepe suzette"
+  }
+  {
+    :title "lemon tart"
+  }
+  {
+    :title "scones"
+  }
+  {
+    :title "bloemkool schotel"
+  }
+  {
+    :title "pasta"
+  }
+  {
+    :title "egg fried rice"
+  }
+  {
+    :title "mince applepies"
+  }
+  {
+    :title "doughnuts"
+  }
+  {
+    :title "oliebollen"
+  }
+  {
+    :title "soda bread"
+  }
+  {
+    :title "schichttorte"
+  }
+  {
+    :title "gnocco fritto"
+  }
+  {
+    :title "coc au bin"
+  }
+  {
+    :title "prue leith's stroopwafels"
+  }
+  {
+    :title "chicken leek pie"
+  }
+  {
+    :title "pizza"
+  }
+  {
+    :title "stained glass biscuits"
   }
 ]
