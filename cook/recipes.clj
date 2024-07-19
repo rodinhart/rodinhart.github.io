5dd4ded520a427fb37d05a0c59bf09a1e9a751d0
Rodin van der Hart <rodin.hart@orgvue.com>
2024-07-19 11:34:33 +0100

    Rough puff pastry

--- cook/recipes.clj
+++ cook/recipes.clj
@@ -63,33 +63,29 @@
     ]
   }
   {
-    :title "cheat's puff pastry"
+    :title "rough puff pastry"
 
     :ingredients [
-      ["300g plain flour"
-      "pinch of salt"
-      "50g butter, in cubes and chilled"]
-      ["120g butter, grated and frozen"
-      "4 to 6 tbsp water"]
+      ["225 plain flour"
+      "175g plant butter, chilled, in small pieces"]
+
+      ["100ml cold water"
+      "pinch of salt"]
     ]
 
     :method [
-      "This is Paul Hollywood's recipe, and it just works.
-      It allows you to make a lovely flaky pastry with a little forthought
-      (freezing and grating the butter), and about 30 minutes of preparation time."
+      "This is from Good Housekeeping."
 
-      "Put the flour, salt and butter cubes in a bowl and break down the butter using
-      fingertips until it resembles breadcrumbs. Without too much kneading,
-      bring it together to form a soft dough."
+      "Sift the flour into a bowl, add the salt. Add the butter. The butter could be cut by hand,
+      use the flour to keep it from sticking, or a food processor can be used. The pieces should be smaller than 1cm.
+      If they are too big, it will leak and prevent layers from forming. Add the water, and bring together without kneading."
 
-      "Roll out the dough into a rectangle about 8cm wide and 24cm high.
-      Put half the grated butter on the bottom two thirds of the rectangle,
-      fold the top third over, and then the bottom third, including the butter, 
-      on top of that. Roll out the dough again, and repeat with the other half of
-      the butter. Roll out a final time, and fold it in the same way but without 
-      putting butter in this time."
+      "Roll out the dough into a rectangle about 8cm wide and 24cm in length. Fold the top third down, then the bottom third
+      on top of that. Turn clock-wise 90 degrees. Wrap in baking paper and chill. Repeat 3 times."
 
-      "Package the dough in baking parchment and leave to chill in the fridge for at least an hour. Done!"
+      "200C for about 30 minutes."
+
+      "Alternative amounts are 180g flour, 140gr butter, 80ml water"
     ]
   }
   {
